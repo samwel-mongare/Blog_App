a6cb41a70acef6f75acb211f3a488df25882d54c
mongare <mongaresams@gmail.com>
2022-03-26 16:15:00 +0300

    Fix errors

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_26_112159) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "posts_connter"
+    t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

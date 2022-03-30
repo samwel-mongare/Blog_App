3770b2a7a25a95f192c0587184ed45a7132a416f
mong'are <mongaresams@gmail.com>
2022-03-30 16:48:17 +0300

    Fix errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :comments, class_name: "comment", foreign_key: "author_id"
-  has_many :posts, class_name: "post", foreign_key: "author_id"
-  has_many :likes, class_name: "like", foreign_key: "author_id"
+  has_many :comments, class_name: 'comment', foreign_key: 'author_id'
+  has_many :posts, class_name: 'post', foreign_key: 'author_id'
+  has_many :likes, class_name: 'like', foreign_key: 'author_id'
 
   def latest_posts
     posts.limit(3).order(created_at: :desc)

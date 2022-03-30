69f30ceaa51f1ad61d2074459123450f2e3436bc
mong'are <mongaresams@gmail.com>
2022-03-30 15:57:11 +0300

    Update all the assosiations in models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, class_name: "like", foreign_key: "post_id"
+  has_many :comments, class_name: "comment", foreign_key: "post_id"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :comments, class_name: "comment", foreign_key: "author_id"
+  has_many :posts, class_name: "post", foreign_key: "author_id"
+  has_many :likes, class_name: "like", foreign_key: "author_id"
 end

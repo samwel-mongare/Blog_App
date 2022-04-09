0160b648e444cf107321dd50b8ac11c5fa043c42
mong'are <mongaresams@gmail.com>
2022-04-09 13:34:36 +0300

    Update the user created success message

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -20,7 +20,7 @@ module Api
       def register
         new_user = User.new(name: params[:name], email: params[:email], password: params[:password])
         if new_user.save
-          render json: { success: true, message: 'User created', data: { user: new_user } }, status: :created
+          render json: { success: true, message: 'User created' }, status: :created
         else
           render json: { success: false, errors: new_user.errors }, status: :unprocessable_entity
         end
